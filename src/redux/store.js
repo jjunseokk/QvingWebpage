31192cbb686be619656b0e80032a237681c181d8
박준석 <parkjunseok@bagjunseog-ui-MacBookAir.local>
2023-03-22 22:48:18 +0900

    gitignore change

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from "redux";
-import thunk from 'redux-thunk' //추가
-import rootReducer from "./reducer";
-
-let store = createStore(rootReducer, applyMiddleware(thunk));
-
-
-export default store;
